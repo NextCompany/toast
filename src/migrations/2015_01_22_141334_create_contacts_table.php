71eb6a174b68206f42223cabaf60a94563dbfe7e
Rafael Guimarães <rafaelguimaraesweb@gmail.com>
2015-01-22 11:16:17 -0300

    Adicionando migration de contacts

---
+++ src/migrations/2015_01_22_141334_create_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('contacts', function($t) {
+			$t->increments('id');
+			$t->string('email', 80);
+			$t->string('nome', 80);
+			$t->text('body');
+
+			$t->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('contacts');
+	}
+
+}
